9e04e91dd14f1e85bfe3afba00378d99cdd93c24
Laid taleb <talebsofiane8@gmail.com>
2023-12-15 17:14:22 +0100

    installing requirements

--- start.sh
+++ start.sh
@@ -3,7 +3,13 @@ cd laradock
 docker-compose up -d workspace postgres nginx
 docker-compose exec workspace bash
 
+//backend
+composer install
+php artisan migrate
+
+//frontend
 cd resources/js/frontend
+npm install
 npm install --save-dev @angular-devkit/build-angular@latest
 npm start -- --host 0.0.0.0
 
